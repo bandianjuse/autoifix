9117db50d101e775a01ac8a31da06c469f9db674
cjsong <157151004@qq.com>
2017-06-22 14:05:42 +0800

    222

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -7,11 +7,11 @@ export default {
   type: 'mysql',
   adapter: {
     mysql: {
-      host: '127.0.0.1',
+      host: '47.91.156.50',
       port: '3306',
       database: 'autoifix',
-      user: 'root',
-      password: '123456',
+      user: 'nethken',
+      password: 'LRz5QKw3iYkUIPUG',
       prefix: 'ai_',
       encoding: 'utf8'
     },
